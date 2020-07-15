8426567de3f9145bee39ead24e1968d58b8e641a
Semyon Pukhov <semyonpuchof@gmail.com>
2020-07-15 11:45:14 +0300

    Little change in linux script

--- run-dev-docker-do-linux.sh
+++ run-dev-docker-do-linux.sh
@@ -7,7 +7,7 @@ docker run \
     --rm \
     -it \
     -v ~/.kube/config:/root/.kube/config \
-    -v ~/.config/doctl/config.yaml:~/.config/doctl/config.yaml \
+    -v ~/.config/doctl/config.yaml:/root/.config/doctl/config.yaml \
     -p 5001:5001 \
     -e NODE_ENV=development \
     kubevious/portable:dev-do
