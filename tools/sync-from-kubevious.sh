fbce2b67134e12cbc5c0b3308c3c5750805d593c
Semyon Pukhov <semyonpuchof@gmail.com>
2021-01-22 17:45:13 +0300

    Update scripts

--- tools/sync-from-kubevious.sh
+++ tools/sync-from-kubevious.sh
@@ -16,24 +16,107 @@ function sync {
     fi
 }
 
-sync "index.js" "index.js"
+sync "index.ts" "index.ts"
+sync "context.ts" "context.ts"
+sync "types.ts" "types.ts"
+sync "version.ts" "version.ts"
 
-sync "lib/snapshot-processor.js" "lib/snapshot-processor.js"
-sync "lib/snapshot-processors/010_parser-alerts.js" "lib/snapshot-processors/010_parser-alerts.js"
-sync "lib/snapshot-processors/200_children-count.js" "lib/snapshot-processors/200_children-count.js"
+#Copying utils
+sync "src/utils/debug-object-logger.ts" "lib/utils/debug-object-logger.ts"
+sync "src/utils/name-helpers.ts" "lib/utils/name-helpers.ts"
+# Finish copying utils
 
-sync "lib/registry/registry.js" "lib/registry/registry.js"
+#Copying snapshot-processor
+sync "src/snapshot-processor/builder.ts" "lib/snapshot-processor/builder.ts"
+sync "src/snapshot-processor/index.ts" "lib/snapshot-processor/index.ts"
 
-sync "lib/search/engine.js" "lib/search/engine.js"
+#Copying snapshot-processor/processor
+sync "src/snapshot-processor/processor/010_parser-alerts.ts" "lib/snapshot-processor/processor/010_parser-alerts.ts"
+sync "src/snapshot-processor/processor/100_rules-engine.ts" "lib/snapshot-processor/processor/100_rules-engine.ts"
+sync "src/snapshot-processor/processor/200_summary.ts" "lib/snapshot-processor/processor/200_summary.ts"
+# Finish copying snapshot-processor/processor
 
-sync "lib/utils/debug-object-logger.js" "lib/utils/debug-object-logger.js"
-sync "lib/utils/name-helpers.js" "lib/utils/name-helpers.js"
+# Finish copying snapshot-processor
 
-sync "lib/routers/top.js" "lib/routers/top.js"
-sync "lib/routers/diagram.js" "lib/routers/diagram.js"
+#Copying server
+sync "src/server/websocket.ts" "lib/server/websocket.ts"
+sync "src/server/index.ts" "lib/server/index.ts"
+# Finish copying server
 
-sync "lib/websocket/server.js" "lib/websocket/server.js"
+#Copying search
+sync "src/search/autocomplete-builder.ts" "lib/search/autocomplete-builder.ts"
+sync "src/search/results.ts" "lib/search/results.ts"
+sync "src/search/engine.ts" "lib/search/engine.ts"
+# Finish copying search
 
+#Copying rule
+sync "src/rule/execution-context.ts" "lib/rule/execution-context.ts"
+sync "src/rule/marker-accessor.ts" "lib/rule/marker-accessor.ts"
+sync "src/rule/marker-cache.ts" "lib/rule/marker-cache.ts"
+sync "src/rule/rule-accessor.ts" "lib/rule/rule-accessor.ts"
+sync "src/rule/rule-cache.ts" "lib/rule/rule-cache.ts"
+sync "src/rule/rule-processor.ts" "lib/rule/rule-processor.ts"
+sync "src/rule/types.ts" "lib/rule/types.ts"
+# Finish copying rule
+
+#Copying routers
+sync "src/routers/collector.ts" "lib/routers/collector.ts"
+sync "src/routers/diagram.ts" "lib/routers/diagram.ts"
+sync "src/routers/history.ts" "lib/routers/history.ts"
+sync "src/routers/marker.ts" "lib/routers/marker.ts"
+sync "src/routers/rule.ts" "lib/routers/rule.ts"
+sync "src/routers/search.ts" "lib/routers/search.ts"
+sync "src/routers/support.ts" "lib/routers/support.ts"
+sync "src/routers/top.ts" "lib/routers/top.ts"
+# Finish copying routers
+
+#Copying registry
+sync "src/registry/registry.ts" "lib/registry/registry.ts"
+# Finish copying registry
+
+#Copying history
+sync "src/history/db-accessor.ts" "lib/history/db-accessor.ts"
+sync "src/history/entities.ts" "lib/history/entities.ts"
+sync "src/history/history-cleanup-processor.ts" "lib/history/history-cleanup-processor.ts"
+sync "src/history/metadata.ts" "lib/history/metadata.ts"
+sync "src/history/processor.ts" "lib/history/processor.ts"
+# Finish copying history
+
+#Copying facade
+sync "src/facade/registry.ts" "lib/facade/registry.ts"
+# Finish copying facade
+
+#Copying collector
+sync "src/collector/collector.ts" "lib/collector/collector.ts"
+# Finish copying facade
+
+#Copying apps
+sync "src/apps/notifications.ts" "lib/apps/notifications.ts"
+# Finish copying apps
+
+#Copying db
+
+#Copying db/meta
+sync "src/db/meta/markers.ts" "lib/db/meta/markers.ts"
+sync "src/db/meta/notifications.ts" "lib/db/meta/notifications.ts"
+sync "src/db/meta/rules.ts" "lib/db/meta/rules.ts"
+# Finish copying db/meta
+
+#Copying db/migrators
+sync "src/db/migrators/1.ts" "lib/db/migrators/1.ts"
+sync "src/db/migrators/2.ts" "lib/db/migrators/2.ts"
+sync "src/db/migrators/3.ts" "lib/db/migrators/3.ts"
+sync "src/db/migrators/4.ts" "lib/db/migrators/4.ts"
+sync "src/db/migrators/5.ts" "lib/db/migrators/5.ts"
+sync "src/db/migrators/6.ts" "lib/db/migrators/6.ts"
+sync "src/db/migrators/7.ts" "lib/db/migrators/7.ts"
+sync "src/db/migrators/8.ts" "lib/db/migrators/8.ts"
+# Finish copying db/migrators
+
+sync "src/db/index.ts" "lib/db/index.ts"
+sync "src/db/migration.ts" "lib/db/migration.ts"
+
+# Finish copying db
 
 if [[ global_status -ne 0 ]]; then
     log_error "[sync-from-kubevious] failed"
--- tools/sync-from-parser.sh
+++ tools/sync-from-parser.sh
@@ -16,27 +16,133 @@ function sync {
     fi
 }
 
-sync "lib/concrete/item.js" "concrete/item.js"
-sync "lib/concrete/registry.js" "concrete/registry.js"
+#Copying concrete
+sync "concrete/item.ts" "concrete/item.ts"
+sync "concrete/registry.ts" "concrete/registry.ts"
+# Finish copying concrete
 
-sync "lib/loaders/k8s.js" "loaders/k8s.js"
+#Copying facade
+sync "facade/registry.ts" "facade/registry.ts"
+# Finish copying facade
 
-sync "lib/logic/helpers" "logic/helpers"
-sync "lib/logic/parsers" "logic/parsers"
-sync "lib/logic/polishers" "logic/polishers"
-sync "lib/logic/scope" "logic/scope"
+#Copying loaders
+sync "loaders/k8s.ts" "loaders/k8s.ts"
+sync "loaders/local.ts" "loaders/local.ts"
+# Finish copying loaders
 
-sync "lib/logic/item.js" "logic/item.js"
-sync "lib/logic/processor.js" "logic/processor.js"
-sync "lib/logic/properties-builder.js" "logic/properties-builder.js"
 
-sync "lib/parsers/api-groups.js" "parsers/api-groups.js"
-sync "lib/parsers/k8s.js" "parsers/k8s.js"
+#Copying logic
 
-sync "lib/utils/debug-object-logger.js" "utils/debug-object-logger.js"
-sync "lib/utils/job-dampener.js" "utils/job-dampener.js"
-sync "lib/utils/name-helpers.js" "utils/name-helpers.js"
+#Copying logic/helpers
+sync "logic/helpers/common.ts" "logic/helpers/common.ts"
+sync "logic/helpers/index.ts" "logic/helpers/index.ts"
+sync "logic/helpers/resources.ts" "logic/helpers/resources.ts"
+sync "logic/helpers/roles.ts" "logic/helpers/roles.ts"
+# Finish copying logic/helpers
 
+#Copying logic/parsers
+sync "logic/parsers/001_scope-all.ts" "logic/parsers/001_scope-all.ts"
+sync "logic/parsers/005_node.ts" "logic/parsers/005_node.ts"
+sync "logic/parsers/010_namespace.ts" "logic/parsers/010_namespace.ts"
+sync "logic/parsers/030_app-controllers.ts" "logic/parsers/030_app-controllers.ts"
+sync "logic/parsers/031_app-controller-cont.ts" "logic/parsers/031_app-controller-cont.ts"
+sync "logic/parsers/032_app-controller-volume.ts" "logic/parsers/032_app-controller-volume.ts"
+sync "logic/parsers/033_app-controller-service-account.ts" "logic/parsers/033_app-controller-service-account.ts"
+sync "logic/parsers/035_autoscaler.ts" "logic/parsers/035_autoscaler.ts"
+sync "logic/parsers/040_service.ts" "logic/parsers/040_service.ts"
+sync "logic/parsers/050_ingress.ts" "logic/parsers/050_ingress.ts"
+sync "logic/parsers/090_replica-set.ts" "logic/parsers/090_replica-set.ts"
+sync "logic/parsers/100_pod.ts" "logic/parsers/100_pod.ts"
+sync "logic/parsers/101_pod_pvc.ts" "logic/parsers/101_pod_pvc.ts"
+sync "logic/parsers/110_config-map.ts" "logic/parsers/110_config-map.ts"
+sync "logic/parsers/110_network-policy.ts" "logic/parsers/110_network-policy.ts"
+sync "logic/parsers/110_persistent-volume-claim.ts" "logic/parsers/110_persistent-volume-claim.ts"
+sync "logic/parsers/110_service-account.ts" "logic/parsers/110_service-account.ts"
+sync "logic/parsers/111_network-policy-used-by.ts" "logic/parsers/111_network-policy-used-by.ts"
+sync "logic/parsers/111_pvc_pv.ts" "logic/parsers/111_pvc_pv.ts"
+sync "logic/parsers/112_network-policies.ts" "logic/parsers/112_network-policies.ts"
+sync "logic/parsers/115_role-init.ts" "logic/parsers/115_role-init.ts"
+sync "logic/parsers/120_role-binding.ts" "logic/parsers/120_role-binding.ts"
+sync "logic/parsers/125_role.ts" "logic/parsers/125_role.ts"
+sync "logic/parsers/130_service-account-role-matrix.ts" "logic/parsers/130_service-account-role-matrix.ts"
+sync "logic/parsers/135_role_psp.ts" "logic/parsers/135_role_psp.ts"
+sync "logic/parsers/136_pod-security-policy.ts" "logic/parsers/136_pod-security-policy.ts"
+# Finish copying logic/parsers
+
+#Copying logic/polishers
+sync "logic/polishers/010_namespace.ts" "logic/parsers/010_namespace.ts"
+sync "logic/polishers/020_nodes-resources.ts" "logic/parsers/020_nodes-resources.ts"
+sync "logic/polishers/030_container-resource.ts" "logic/parsers/030_container-resource.ts"
+sync "logic/polishers/031_app-controller-resources.ts" "logic/parsers/031_app-controller-resources.ts"
+sync "logic/polishers/032_namespace-resources.ts" "logic/parsers/032_namespace-resources.ts"
+sync "logic/polishers/033_container-radioactive.ts" "logic/parsers/033_container-radioactive.ts"
+sync "logic/polishers/033_launcher-radioactive.ts" "logic/parsers/033_launcher-radioactive.ts"
+sync "logic/polishers/033_pod-radioactive.ts" "logic/parsers/033_pod-radioactive.ts"
+sync "logic/polishers/040_storage-class.ts" "logic/parsers/040_storage-class.ts"
+sync "logic/polishers/041_storage.ts" "logic/parsers/041_storage.ts"
+# Finish copying logic/polishers
+
+#Copying logic/processor
+sync "logic/processor/base/builder.ts" "logic/processor/base/builder.ts"
+sync "logic/processor/base/executor.ts" "logic/processor/base/executor.ts"
+sync "logic/processor/concrete/executor.ts" "logic/processor/concrete/executor.ts"
+sync "logic/processor/concrete/executor.ts" "logic/processor/concrete/executor.ts"
+sync "logic/processor/concrete/handler-args.ts" "logic/processor/concrete/handler-args.ts"
+sync "logic/processor/logic/executor.ts" "logic/processor/logic/executor.ts"
+sync "logic/processor/logic/executor.ts" "logic/processor/logic/executor.ts"
+sync "logic/processor/logic/handler-args.ts" "logic/processor/logic/handler-args.ts"
+sync "logic/processor/scope/executor.ts" "logic/processor/scope/executor.ts"
+sync "logic/processor/scope/executor.ts" "logic/processor/scope/executor.ts"
+sync "logic/processor/scope/handler-args.ts" "logic/processor/scope/handler-args.ts"
+sync "logic/processor/index.ts" "logic/processor/index.ts"
+sync "logic/processor/types.ts" "logic/processor/types.ts"
+# Finish copying logic/processor
+
+#Copying logic/scope
+sync "logic/scope/app.ts" "logic/scope/app.ts"
+sync "logic/scope/index.ts" "logic/scope/index.ts"
+sync "logic/scope/infra.ts" "logic/scope/infra.ts"
+sync "logic/scope/item.ts" "logic/scope/item.ts"
+sync "logic/scope/items.ts" "logic/scope/items.ts"
+sync "logic/scope/label-matcher.ts" "logic/scope/label-matcher.ts"
+sync "logic/scope/namespace.ts" "logic/scope/namespace.ts"
+# Finish copying logic/scope
+
+sync "logic/item.ts" "logic/item.ts"
+sync "logic/parser-builder.ts" "logic/parser-builder.ts"
+sync "logic/properties-builder.ts" "logic/properties-builder.ts"
+sync "logic/table-builder.ts" "logic/table-builder.ts"
+# Finish copying logic
+
+#Copying parsers
+sync "parsers/api-groups.ts" "parsers/api-groups.ts"
+sync "parsers/k8s.ts" "parsers/k8s.ts"
+# Finish copying parsers
+
+#Copying reporting
+sync "reporting/reporter.ts" "reporting/reporter.ts"
+sync "reporting/snapshot-reporter.ts" "reporting/snapshot-reporter.ts"
+sync "reporting/snapshot.ts" "reporting/snapshot.ts"
+sync "reporting/target.ts" "reporting/target.ts"
+# Finish copying reporting
+
+#Copying routers
+sync "routers/top.ts" "routers/top.ts"
+# Finish copying routers
+
+#Copying server
+sync "server/index.ts" "server/index.ts"
+# Finish copying server
+
+#Copying server
+sync "utils/debug-object-logger.ts" "utils/debug-object-logger.ts"
+sync "utils/job-dampener.ts" "utils/job-dampener.ts"
+sync "utils/name-helpers.ts" "utils/name-helpers.ts"
+# Finish copying server
+
+sync "context.ts" "context.ts"
+sync "index.ts" "index.ts"
+sync "version.ts" "version.ts"
 if [[ global_status -ne 0 ]]; then
     log_error "[sync-from-parser] failed"
 fi
