5c0f1706405008b408cfb4a978f68d76c4fd21df
Semyon Pukhov <semyonpuchof@gmail.com>
2021-02-04 10:49:40 +0300

    Remove mock-data

--- src/mock-data/data/samples/deploy.sh
+++
@@ -1,8 +0,0 @@
-kubectl apply -f 00-init/
-kubectl apply -f 01-ingress-isolated-ns/
-kubectl apply -f 02-ingress-isolated-ns/
-kubectl apply -f 03-ingress-isolated-pod/
-kubectl apply -f 04-egress-isolated-ns/
-kubectl apply -f 05-egress-isolated-pod/
-kubectl apply -f 06-multi-ns-web-app/
-
