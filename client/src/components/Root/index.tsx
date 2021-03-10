3665983264f71d2a70f48c7c58ad02092aa4f5cd
Semyon Pukhov <semyonpuchof@gmail.com>
2021-03-10 17:34:28 +0300

    Minor fix

--- client/src/components/Root/index.tsx
+++ client/src/components/Root/index.tsx
@@ -4,8 +4,8 @@ import { ClassComponent } from "@kubevious/ui-framework"
 import { FieldsSaver } from "../../utils/save-fields"
 import { ErrorBox, Popup } from '@kubevious/ui-components';
 import { GoldenLayout } from '@kubevious/ui-components';
-import { Header } from "../Header"
-import { SEO } from "../SEO"
+import Header from "../Header"
+import SEO from "../SEO"
 import ClusterScreen from '../ClusterScreen'
 import { RootState } from "./types"
 import { sharedState } from "../../configureService";
